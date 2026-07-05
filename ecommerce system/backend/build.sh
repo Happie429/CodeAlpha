32c42be207a705875f1ad8ab3daf6f18a5032c2b
Happie429 <mehappie0@gmail.com>
2026-07-06 00:41:23 +0530

    Create Render superuser automatically

--- ecommerce system/backend/build.sh
+++ ecommerce system/backend/build.sh
@@ -5,3 +5,19 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        "admin",
+        "admin@example.com",
+        "Admin@12345"
+    )
+    print("Superuser created!")
+else:
+    print("Superuser already exists!")
+EOF
